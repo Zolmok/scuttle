d8422e83b2ac7816aca9cfa38000f738a18730b2
Ricky Nelson <rickyn@zolmok.org>
2022-07-09 17:01:57 -0400

    Move run_apps to respective callers

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,11 @@
-use std::fmt::{self, Formatter, Display};
 use std::process::Command;
 
-struct Args(Vec<String>);
 pub struct App {
     pub command: String,
     pub args: Vec<String>
 }
 
-impl Display for Args {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        write!(f, "{}", self.0.join(" "))
-    }
-}
-
 pub fn run_app<'a>(app: &App) -> Result<std::process::ExitStatus, std::io::Error> {
-    println!("");
-    println!("========================");
-    println!("$ {} {}", app.command, Args(app.args.to_owned()));
-    println!("========================");
-
     let child_result = Command::new(app.command.clone())
         .args(app.args.clone())
         .spawn();
@@ -32,16 +19,6 @@ pub fn run_app<'a>(app: &App) -> Result<std::process::ExitStatus, std::io::Error
         Err(error) => Err(error),
     }
 }
-
-pub fn run_apps(apps: &[App]) {
-    for app in apps.iter() {
-        match run_app(app) {
-            Err(error) => panic!("panic{}", error),
-            Ok(_status) => continue,
-        };
-    }
-}
-
 
 #[cfg(test)]
 mod tests {
